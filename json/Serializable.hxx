8ed88b50f6e89a6c3a11db6886e008684eeed6fe
Jene Litsch <litsch.jene@gmail.com>
2021-10-27 10:40:05 +0200

    Updated Serializable Interface

--- json/Serializable.hxx
+++ json/Serializable.hxx
@@ -4,17 +4,7 @@
 namespace json {
 	class Serializable {
 	public:
-		virtual void readJson(std::istream & in) = 0;
-		virtual void writeJson(std::ostream & out) const = 0;
+		virtual void readJson(const json::Value & value) = 0;
+		virtual std::unique_ptr<Value> writeJson() const = 0;
 	};
-	
-	std::istream & operator>>(std::istream & in, Serializable & obj) {
-		obj.readJson(in);
-		return in;
-	}	
-
-	std::ostream & operator<<(std::ostream & out, const Serializable & obj) {
-		obj.writeJson(out);
-		return out;
-	}
 }
